8c655df732a6a4fd999386c63dc6a95226488da8
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-09 23:53:42 +0530

    First commit of arraylist program

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class ArrayListOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+           int n = Integer.parseInt(scanner.nextLine());
+        String[] items = scanner.nextLine().split(" ");
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(items[i]);
+        }
+
+        // Sort list
+        Collections.sort(list);
+
+        // Search item
+        String searchItem = scanner.nextLine();
+
+        // Output
+        System.out.println("Sorted Items: " + list);
+
+        if (list.contains(searchItem)) {
+            System.out.print("Found");
+        } else {
+            System.out.print("Not Found");
+        }
     }
 }
+    
+
